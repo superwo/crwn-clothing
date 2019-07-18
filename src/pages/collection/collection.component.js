86badd891d7de87fee5f0123476e6df6da6c54d1
Radu <avadanii@tiscali.it>
2019-07-18 10:49:46 +0300

    nested routing in shop page

---
+++ src/pages/collection/collection.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className='collection-page'>
+    <h2>COLLECTION PAGE</h2>
+    <p>{match.params.collectionId}</p>
+  </div>
+);
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
